8366f2b2f36cbaae550fe89a80f7fa89f38b757b
Limdongdang <dio8620@naver.com>
2023-10-19 11:22:35 +0900

    updata math.py

---
+++ math.py
@@ -0,0 +1,23 @@
+# 두 숫자를 입력받습니다.
+num1 = float(input("첫 번째 숫자를 입력하세요: "))
+num2 = float(input("두 번째 숫자를 입력하세요: "))
+
+# 덧셈
+addition = num1 + num2
+print("덧셈 결과:", addition)
+
+# 뺄셈
+subtraction = num1 - num2
+print("뺄셈 결과:", subtraction)
+
+# 곱셈
+multiplication = num1 * num2
+print("곱셈 결과:", multiplication)
+
+# 나눗셈
+if num2 != 0:
+    division = num1 / num2
+    print("나눗셈 결과:", division)
+else:
+    print("0으로 나눌 수 없습니다.")
+
